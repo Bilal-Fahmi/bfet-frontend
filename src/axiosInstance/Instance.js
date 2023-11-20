e9d64ff277f2f2384e8dbbf65556f5dc027af7b6
Bilal-Fahmi <bilalfahmiarif@gmail.com>
2023-11-20 22:15:43 +0300

    instance update

--- src/axiosInstance/Instance.js
+++ src/axiosInstance/Instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://bfet-server.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
